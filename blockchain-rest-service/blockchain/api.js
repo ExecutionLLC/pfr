2a1736d37058a6ca22f5125e76e2c7c0f4974345
MadHatter <ivkalgin@gmail.com>
2017-09-28 13:34:37 +0700

    removed unused code, added changeNpf in Api

--- blockchain-rest-service/blockchain/api.js
+++ blockchain-rest-service/blockchain/api.js
@@ -6,116 +6,6 @@ const config = require('../utils/config');
 const logger = require('../utils/log')('blockchain api');
 const validator = require('../utils/validator');
 
-// function _newWeb3Instance() {
-//     const provider = new Web3.providers.HttpProvider(config.get('ethNodeUrl'));
-//     return new Web3(provider);
-// }
-//
-// function _getContractInstance(web3Instance) {
-//     return new web3Instance.eth.Contract(CONTRACT.ABI, CONTRACT.ID);
-// }
-//
-// function _getEthWallet(privateKey) {
-//     if (!validator.isValidPrivateKey(privateKey)) {
-//         throw Error('user private key is not valid');
-//     }
-//     const chainId = config.get('ethNodeChainId') || 1;
-//     const provider = new ethers.providers.JsonRpcProvider(config.get('ethNodeUrl'), false, chainId);
-//     return new ethers.Wallet(privateKey, provider);
-// }
-//
-// function _getSignedContract(privateKey) {
-//     const wallet = _getEthWallet(privateKey);
-//     return ethers.Contract(CONTRACT.ABI, CONTRACT.ID, wallet);
-// }
-//
-// const Api = {
-//     getNpfList() {
-//         const web3 = _newWeb3Instance();
-//         const contractInstance = _getContractInstance(web3);
-//
-//         const options = {
-//             fromBlock: 0,
-//             toBlock: 'latest'
-//         };
-//
-//         return contractInstance.getPastEvents('EventNewNpf', options).then((logs) => {
-//             return logs.map(log => {
-//                 const {_name: name, _owner: owner} = log.returnValues;
-//                 return {
-//                     name,
-//                     owner,
-//                     transactionHash: log.transactionHash
-//                 }
-//             });
-//         });
-//     },
-//     getPersonInfoBySnils(snils) {
-//         const web3 = _newWeb3Instance();
-//         const contractInstance = _getContractInstance(web3);
-//         return contractInstance.methods.personInfo(snils).call().then((result) => {
-//             return {
-//                 npf: result.npf,
-//                 tariff: result.tariff
-//             };
-//         });
-//     },
-//     getPersonInfoByAddress(address) {
-//         if (!validator.isValidWalletId(address)) {
-//             throw Error('user address is not valid');
-//         }
-//         const web3 = _newWeb3Instance();
-//         const contractInstance = _getContractInstance(web3);
-//         return contractInstance.methods.personInfoByAddress(address).call().then((result) => {
-//             return {
-//                 npf: result.npf,
-//                 tariff: result.tariff
-//             };
-//         });
-//     },
-//     changeTariff(privateKey, tariff) {
-//         const contract = _getSignedContract(privateKey);
-//         return contract.changeTariff(tariff).then((transaction) => {
-//             return {
-//                 transactionHash: transaction.hash
-//             };
-//         });
-//     },
-//     getOperationsList(address) {
-//         if (!validator.isValidWalletId(address)) {
-//             throw Error('user address is not valid');
-//         }
-//
-//         const web3 = _newWeb3Instance();
-//         const contractInstance = _getContractInstance(web3);
-//
-//         const options = {
-//             filter: { _owner: address },
-//             fromBlock: 0,
-//             toBlock: 'latest'
-//         };
-//
-//         return contractInstance.getPastEvents('EventOperation', options).then((logs) => {
-//             return logs.map(log => {
-//                 const {
-//                     _npf: npf,
-//                     _timestamp: timestamp,
-//                     _amount: amount,
-//                     _comment: comment
-//                 } = log.returnValues;
-//
-//                 return {
-//                     npf,
-//                     timestamp,
-//                     amount,
-//                     comment,
-//                     transactionHash: log.transactionHash
-//                 }
-//             });
-//         });
-//     }
-// };
-
 class BlockchainApi {
     constructor() {
         const provider = new Web3.providers.HttpProvider(config.get('ethNodeUrl'));
@@ -203,7 +93,7 @@ class BlockchainApi {
         return Promise.all([this._initNpfCache(), this._initOperationsCache()]);
     }
 
-    _getEthWallet(privateKey) {
+    static _getEthWallet(privateKey) {
         if (!validator.isValidPrivateKey(privateKey)) {
             throw Error('private key is not valid');
         }
@@ -213,7 +103,7 @@ class BlockchainApi {
     }
 
     _getSignedContract(privateKey) {
-        const wallet = this._getEthWallet(privateKey);
+        const wallet = _getEthWallet(privateKey);
         return new ethers.Contract(CONTRACT.ID, CONTRACT.ABI, wallet);
     }
 
@@ -255,6 +145,15 @@ class BlockchainApi {
     changeTariff(privateKey, tariff) {
         const contract = this._getSignedContract(privateKey);
         return contract.changeTariff(tariff).then((transaction) => {
+            return {
+                transactionHash: transaction.hash
+            };
+        });
+    }
+
+    changeNpf(privateKey, npfAddress) {
+        const contract = this._getSignedContract(privateKey);
+        return contract.changeNpf(npfAddress).then((transaction) => {
             return {
                 transactionHash: transaction.hash
             };
