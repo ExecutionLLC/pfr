dbc75602dfc9f71e62fbc6c265be8efd2cec0b34
Igor <grammatchikov@inbox.ru>
2017-10-26 17:19:43 +0300

    поменяли переприсваивание переменой

--- ui-frontend/controller/TabBarControllers/NPF.controller.js
+++ ui-frontend/controller/TabBarControllers/NPF.controller.js
@@ -67,7 +67,7 @@ sap.ui.define([
             var npfHistory = this.oMainModel.getProperty("/npfHistory");
             var pendedNpfChanges = this.oMainModel.getProperty("/pendedNpfChanges");
 
-            var npfTableData = pendedNpfChanges.map(function (value) {
+            var pendedNpfTableData = pendedNpfChanges.map(function (value) {
                 return {
                     npf: value.npf,
                     timestamp: value.timestamp,
@@ -75,15 +75,16 @@ sap.ui.define([
                     isFinished: false
                 };
             });
-            npfTableData = npfTableData.concat(npfHistory.map(function (value) {
+            var historyNpfTableData = npfHistory.map(function (value) {
                 return {
                     npf: value.newNpf,
                     timestamp: value.timestamp,
                     transactionHash: value.transactionHash,
                     isFinished: true
                 }
-            }));
-            this.oTechModel.setProperty('/tech/changeNpfTab/npfTableData', npfTableData);
+            });
+            var totalnpfTableData = pendedNpfTableData.concat(historyNpfTableData);
+            this.oTechModel.setProperty('/tech/changeNpfTab/npfTableData', totalnpfTableData);
 
             if (pendedNpfChanges.length !== 0) {
                 this.oTechModel.setProperty("/tech/changeNpfTab/selectedNpf", "");
