70821fcc7d3ff362c2b83b3ade19887890e445f0
MadHatter <ivkalgin@gmail.com>
2017-11-03 16:32:51 +0700

    now env variables have max priority in config, added transformation function of env variables to settings variables

--- blockchain-rest-service/utils/config.js
+++ blockchain-rest-service/utils/config.js
@@ -1,6 +1,26 @@
 const nconf = require('nconf');
+const changeCase = require('change-case');
 
-nconf.argv().env();
-nconf.file({ file: 'config.json' });
+const ENV_VARIABLES_PREFIX = 'PFR_APP_';
+
+function transformEnvVariable(keyValueObj) {
+    const { key, value } = keyValueObj;
+    const minKeyLength = ENV_VARIABLES_PREFIX.length + 1;
+    if (!key || !key.startsWith(ENV_VARIABLES_PREFIX) || key.length < minKeyLength) {
+        return false;
+    }
+
+    return {
+        key: changeCase.camelCase(key.splice(8)),
+        value
+    };
+}
+
+nconf.file({
+    file: 'default-config.json'
+}).env({
+    transform: transformEnvVariable,
+    parseValues: true
+}).argv();
 
 module.exports = nconf;
